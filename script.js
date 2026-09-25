3bf19f5c8013e0f8a320df87f1c6c478a4930802
pip-ed <pippa.i.edwards@gmail.com>
2026-09-25 15:54:20 +0100

    Add files via upload
    Initial deploy

---
+++ script.js
@@ -0,0 +1,12 @@
+/*
+    Site-wide JavaScript
+
+    Keep this file for small interactive features that you may
+    add later. The site currently works entirely without JavaScript.
+*/
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    // Your JavaScript can go here later.
+
+});
